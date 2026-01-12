191d861ec0af2ce1dbb2b4f9d32d1606839d112b
ku63026 <ku63026@kingsston.ac.uk>
2026-01-12 02:14:15 +0000

    Added the MainViewer extends JPanel content pane structure

--- src/main/java/userprofilebuilder/view/MainViewer.java
+++ src/main/java/userprofilebuilder/view/MainViewer.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package userprofilebuilder.view;
+import javax.swing.JPanel;
 
 /**
  * MainViewer is the *root* UI container for the application.
@@ -11,6 +9,6 @@ package userprofilebuilder.view;
  * The App class creates a JFrame and sets its content pane to an instance
  * of this panel.
  */
-public class MainViewer {
+public class MainViewer extends JPanel{
     
 }
