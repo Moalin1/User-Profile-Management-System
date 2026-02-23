6e790c44981db4873d919bd5e3b8b6aadc2d9e52
abuba <abuba@moalin.home>
2026-02-23 13:11:46 +0000

    Completed WR1 - Basic Main Viewer Window & WR2 - Add Layout and Name 'Edit' Panel.

--- src/main/java/userprofilebuilder/view/MainViewer.java
+++ src/main/java/userprofilebuilder/view/MainViewer.java
@@ -1,6 +1,9 @@
 
 package userprofilebuilder.view;
 import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.TitledBorder;
 
 /**
  * MainViewer is the *root* UI container for the application.
@@ -13,5 +16,21 @@ import javax.swing.JPanel;
 // Week 1 in W1R1 you will need to set the attributes of the frame in App.java
 // Also to add an EditPanel in MainViewer for the components to go in.
 public class MainViewer extends JPanel{
+    public MainViewer() {
+        
+        setLayout (new BorderLayout());
+        
+        JPanel nameEditPanel = new JPanel();
+        nameEditPanel.setBorder(new TitledBorder("Name"));
+        
+        JTabbedPane tabs = new JTabbedPane();
+        
+
+        
+        tabs.addTab("User Name", nameEditPanel);        //  Creating new tabs
+        
+        
+        add(tabs, BorderLayout.CENTER);         //  Edits the layout of all the tabs and content in it
+    }
     
 }
